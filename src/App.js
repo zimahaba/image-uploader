a6775e8262771fe611f652f02de8876a850911d9
zimahaba <pmaquitorres@gmail.com>
2023-04-13 10:13:24 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+const FormData = require('form-data');
+
+const api = axios.create({
+  baseURL: "http://localhost:3001"
+});
 
 function App() {
+
+  const [file, setFile] = useState();
+  const [fileUploaded, setFileUploaded] = useState(false);
+  const [imgUrl, setImgUrl] = useState('');
+
+  const selectImageHandler = (event) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const upload = async () => {
+
+    const form = new FormData();
+    form.append('title', file.name);
+    form.append('file', file);
+
+    axios.post('http://localhost:3001/upload', form, {
+      responseType: 'blob'
+    })
+    .then((res) => {
+      const href = URL.createObjectURL(res.data);
+
+      setFileUploaded(true);
+      setImgUrl(href);
+    })
+    .catch((err) => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <input type='file' onChange={selectImageHandler}/>
+      <button onClick={upload}>Upload</button>
+      {fileUploaded && 
+        <div>
+          <img style={{width: '200px'}} src={imgUrl}/>
+        </div>
+      }
     </div>
   );
 }
